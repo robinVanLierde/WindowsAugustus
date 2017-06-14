ce85bc525bdc5a80e3fb3af402dad867cecab52b
robin van Lierde <robin2383@hotmail.com>
2017-06-14 10:17:22 +0200

    nieuwe api

---
+++ Api/Models/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Api.Models
+{
+    public class Student
+    {
+        public string Naam { get; set; }
+        public string Straat { get; set; }
+        public string Gemeente { get; set; }
+        public int HuisNr { get; set; }
+        public string Email { get; set; }
+        public string Opleiding { get; set; }
+        public string Campus { get; set; }
+
+
+    }
+}
